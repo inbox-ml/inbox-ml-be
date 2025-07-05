ce03732c3b6a89420ed18a3a29b732e42be69e2c
Alex Flok <alexflok2018@gmail.com>
2025-07-05 13:24:12 -0700

    igonore token check if access docs page;

--- middleware/auth.py
+++ middleware/auth.py
@@ -3,6 +3,10 @@ from starlette.middleware.base import BaseHTTPMiddleware
 
 class AuthTokenMiddleWare(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
+
+        if request.url.path in ["/docs", "/redoc", "/openapi.json"]:
+            return await call_next(request)
+
         token = request.headers.get("Authorization")
         if token is None or not token.startswith("Bearer"):
             raise HTTPException(status_code=401, detail="Missing or invalid Authorization header")
